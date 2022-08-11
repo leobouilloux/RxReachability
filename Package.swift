550f64db55025c3fd4a5db8515d55d287bc3c5bf
Leo <leo.bouilloux@gmail.com>
2022-08-11 15:57:33 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,6 +13,7 @@ let package = Package(
     products: [
         .library(
             name: "RxReachability",
+            type: .dynamic,
             targets: ["RxReachability"])
     ],
     dependencies: [
